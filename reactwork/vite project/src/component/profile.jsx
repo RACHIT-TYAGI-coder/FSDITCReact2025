9ec59ff3bf4d54e7bee36c4a896149fcf5dd5258
RACHIT-TYAGI-coder <rachittyagi287@gmail.com>
2025-10-27 13:11:12 +0530

    vite

---
+++ reactwork/vite project/src/component/profile.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function profile(){
+    return(
+        <div>
+            <h3>This is MY Profile</h3>
+            <img src=''height={200} width={200} alt="profile pic"></img>
+        </div>
+    )
+}
+export default profile
